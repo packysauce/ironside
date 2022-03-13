5787bdff18a89ff51dfaeaa000a4e8e7269361bd
Patrick White <1275985+packysauce@users.noreply.github.com>
2022-03-13 15:28:45 -0700

    simple round trip unit tester

--- src/lib.rs
+++ src/lib.rs
@@ -18,22 +18,25 @@ mod ffi {
 #[cfg(test)]
 #[test]
 fn test_encode_int() {
-    let mut buf = vec![0u8; 64];
-    my_encode_int(&mut buf, 1234)
+    let encode_me = 1234;
+    let out = my_encode_int(encode_me);
+    let mut new_out = Vec::new();
+    let r = vlq::encode(encode_me.into(), &mut new_out).unwrap();
+    assert_eq!(out, new_out);
 }
 
-fn my_encode_int(buf: &mut [u8], v: u32) {
-    dbg!(&buf);
-    let ended = unsafe {
-        let mangle_me = buf.as_mut_ptr();
-        let copy_of_ptr = mangle_me;
-        let returned = ffi::command::encode_int(mangle_me, v);
-        println!(
-            "mangle: {:?}, copy: {:?}, returned: {:?}",
-            mangle_me, copy_of_ptr, returned
+fn my_encode_int(v: u32) -> Vec<u8> {
+    let mut buf = [0u8; 10];
+    let buf_start = buf.as_mut_ptr();
+    let count = unsafe {
+        let buf_end = ffi::command::encode_int(buf_start, v);
+        assert!(
+            buf_end > buf_start,
+            "encode_int returned a pointer with negative offset"
         );
+        buf_end.offset_from(buf_start) as usize
     };
-    dbg!(&buf);
+    buf[..count].into()
 }
 
 mod cli;
