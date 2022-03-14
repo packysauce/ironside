b8877009263b115d98be63a47b51b153f541f0f3
Patrick White <1275985+packysauce@users.noreply.github.com>
2022-03-13 20:07:59 -0700

    woo! round trip unit tests!

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,8 @@
 use std::fmt::Display;
 use std::marker::PhantomData;
 
+use ffi::command::parse_int;
+
 //use pyo3::prelude::*;
 
 mod ffi {
@@ -16,18 +18,63 @@ mod ffi {
 }
 
 #[cfg(test)]
-#[test]
-fn test_encode_int() {
-    let encode_me = 1234;
-    let out = my_encode_int(encode_me);
-    let mut new_out = Vec::new();
-    let r = vlq::encode(encode_me.into(), &mut new_out).unwrap();
-    assert_eq!(out, new_out);
+mod tests {
+    use super::*;
+    use proptest::prelude::*;
+    use proptest::num::u32::ANY;
+
+    proptest!{
+
+        #[test]
+        fn encode_handles_all_numbers(value in ANY) {
+            let r = encode_int(value);
+            // https://www.klipper3d.org/Protocol.html#variable-length-quantities
+            let expected_len = match value as i32 {
+                -32 ..= 95 => 1,
+                -4096 ..= 12_287 => 2,
+                -524_288 ..= 1_572_863 => 3,
+                -67_108_864 ..= 201_326_591 => 4,
+                _ => 5,
+            };
+            assert_eq!(expected_len, r.len());
+        }
+    }
+
+    /// Check that a number survives the encode/decode round trip
+    #[test]
+    fn test_round_trip() {
+        let mut count = 0;
+        for _ in 0u32..1000 {
+            count += 1;
+            let encode_me = rand::random();
+            let decode_me = encode_int(encode_me);
+            let decoded = decode_int(&decode_me);
+            assert_eq!(encode_me, decoded, "failed on attempt #{}", count);
+        }
+    }
+
+    #[test]
+    fn test_encode_one() {
+        assert_eq!(encode_int(1234), &[0x89, 0x52]);
+    }
+
+    #[test]
+    fn test_decode_one() {
+        assert_eq!(decode_int(&[0x89, 0x52]), 1234);
+    }
 }
 
-fn my_encode_int(v: u32) -> Vec<u8> {
-    let mut buf = [0u8; 10];
-    let buf_start = buf.as_mut_ptr();
+pub fn decode_int(data: &[u8]) -> u32 {
+    let mut copy_of_data = data.to_vec();
+    let mut get_mangled = copy_of_data.as_mut_ptr();
+    //let out = unsafe { parse_int(data.as_mut_slice() as *mut [u8]) };
+    let out = unsafe { parse_int(&mut get_mangled) };
+    out
+}
+
+pub fn encode_int(v: u32) -> Vec<u8> {
+    let mut buf = [0x81u8; 10]; // blub
+    let mut buf_start = buf.as_mut_ptr();
     let count = unsafe {
         let buf_end = ffi::command::encode_int(buf_start, v);
         assert!(
