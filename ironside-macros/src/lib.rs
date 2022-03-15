587a9a4c14c697f4c0b4195837a851fe493486d6
Patrick White <1275985+packysauce@users.noreply.github.com>
2022-03-15 16:43:32 -0700

    add a macro crate

---
+++ ironside-macros/src/lib.rs
@@ -0,0 +1,48 @@
+use darling::ast::Data;
+use darling::{FromDeriveInput, FromField};
+use proc_macro::TokenStream;
+use quote::quote;
+use syn::{parse_macro_input, DeriveInput, Field, Ident, Type};
+
+#[derive(FromDeriveInput)]
+#[darling(attributes(cmd), supports(struct_named))]
+struct Command {
+    ident: Ident,
+    generics: syn::Generics,
+    data: Data<(), CommandField>,
+    /// OID used to identify the command between host and MCU
+    oid: u32,
+    /// Name of the reponse type this command
+    response: Option<String>,
+}
+
+#[derive(FromField)]
+struct CommandField {
+    // These come from the FromField derive, they're magic
+    ident: Option<Ident>,
+    ty: Type,
+    // These come from our #[cmd(rename=blah, response=Blah)]
+    response: Type,
+    rename: Option<&'static str>,
+}
+
+#[proc_macro_derive(Command, attributes(klip))]
+pub fn command(stream: TokenStream) -> TokenStream {
+    let input = parse_macro_input!(stream as DeriveInput);
+    let Command {
+        ident,
+        generics,
+        data,
+        oid,
+        response,
+    } = Command::from_derive_input(&input).unwrap();
+    let resp_type = response.or_else(|| Some("()".to_string()));
+
+    quote! {
+        impl Request for #ident {
+            const OID: u32 = #oid;
+            pub type Resp = ();
+        }
+    }
+    .into()
+}
